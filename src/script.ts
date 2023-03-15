d0916bc7d7f9235b607cfa9d48acdb03eaa1121d
andrewinci <andrewinci@gmail.com>
2023-03-15 12:46:52 +0000

    feat: add messages and receiver to the script

--- src/script.ts
+++ src/script.ts
@@ -1,7 +1,39 @@
 // entry point for the content-script loaded in the web page
 // the application running in the pop-up can communicate with this script
+
+import { PopUpMessage, ScriptMessage } from "./messages";
+
 // via chrome messaging
+const LOCAL_STORAGE_KEY = "excalidraw";
 
-export const sample = () => console.log("Hello");
+// logic to handle messages from the extension popup
+chrome.runtime.onMessage.addListener(function (
+  genericRequest,
+  _,
+  sendResponse
+) {
+  const request = genericRequest as PopUpMessage;
+  const reply = (message: ScriptMessage) => sendResponse(message);
+  switch (request.action) {
+    case "ping":
+      reply({ action: "pong" });
+      break;
+    case "get-drawing":
+      reply({
+        action: "drawing",
+        data: localStorage.getItem(LOCAL_STORAGE_KEY),
+      });
+      break;
+    case "set-drawing":
+      localStorage.setItem(LOCAL_STORAGE_KEY, request.data);
+      reply({
+        action: "drawing-set",
+        success: true,
+      });
+      // refresh the page to load the new content
+      window.location.reload();
+      break;
+  }
+});
 
-sample();
+export {};
